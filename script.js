b3b580af4deab31c400c8edc1a677c905c2362a0
KuluHV <hiverlab.kyleasuncion@gmail.com>
2022-03-17 15:23:29 +0800

    Background image fix v0.0

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//======
+//======VARIABLES======
+//======
 var loader = $('#loader');
 
+//===Loading===
 window.addEventListener("load",function(){
     loader.fadeOut(1000);
     document.body.style.overflow = "unset";
